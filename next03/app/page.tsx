7ae1edab3a16db522ef7fac99ead65c2d7f0cb08
Suntoh <weerawit.j@gmail.com>
2024-10-11 20:00:54 +0700

    next project03-wikiSearch

---
+++ next03/app/page.tsx
@@ -0,0 +1,13 @@
+import { Inter } from "next/font/google"
+import Navbar from "./components/Navbar"
+
+const inter = Inter({subsets:['latin']})
+
+export default function Home() {
+  return (
+    <main className={inter.className}>
+      {/* <Navbar /> */}
+    
+    </main>
+  )
+}
